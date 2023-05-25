c90e1df1a49828ccc55e16839b591fe9f3002d8b
unknown <lluvporwall@gmail.com>
2023-05-25 15:30:25 +0530

    ok

---
+++ server.js
@@ -0,0 +1,9 @@
+const jsonServer =require("json-server");
+const server = jsonServer.create()
+const route = jsonServer.router("db.json")
+const middlewares = jsonServer.defaults();
+const port =3000
+
+server.use(middlewares)
+server.use(router)
+server.listen(port)
